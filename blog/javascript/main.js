c4816587e443272b6359f33b53c9ca84f894c945
doh17003@byui.edu <66549514+DevPrestigious@users.noreply.github.com>
2022-12-01 15:58:55 +0000

    Site Improvements

--- blog/javascript/main.js
+++ blog/javascript/main.js
@@ -1,5 +1,5 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
+function myTopNavFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
@@ -7,3 +7,20 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+function myBottomNavFunction() {
+  var x = document.getElementById("myBottomnav");
+  if (x.className === "bottomnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "bottomnav";
+  }
+}
+
+const paragraph = `
+<p>
+  Copyright &copy; ${new Date().getFullYear()} Sanya's Blog | Alexander Dohms - WDD130
+</p>
+`;
+
+document.getElementById('copyright').innerHTML = paragraph;
